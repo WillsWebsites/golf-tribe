b440f0ca71adc567dbafdc165c79f30595106767
--replace-all <contactwillswebsites@gmail.com>
2022-08-19 18:36:03 -0600

    Fixing a lot of stuff and a ton of refactoring

--- src/js/home.js
+++ src/js/home.js
@@ -1,21 +1,26 @@
 // Modal popups
-function modalFunction() {
-  let popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-  popup.parentElement.classList.toggle("popupOpened");
-}
+// function modalFunction() {
+//   let popup = document.getElementById("myPopup");
+//   popup.classList.toggle("show");
+//   popup.parentElement.classList.toggle("popupOpened");
+// }
 
-function modalFunction2() {
-  let popup = document.getElementById("myPopup2");
-  popup.classList.toggle("show");
-  popup.parentElement.classList.toggle("popupOpened");
-}
+// function modalFunction2() {
+//   let popup = document.getElementById("myPopup2");
+//   popup.classList.toggle("show");
+//   popup.parentElement.classList.toggle("popupOpened");
+// }
 
-function modalFunction3() {
-  let popup = document.getElementById("myPopup3");
-  popup.classList.toggle("show");
-  popup.parentElement.classList.toggle("popupOpened");
-}
+// function modalFunction3() {
+//   let popup = document.getElementById("myPopup3");
+//   popup.classList.toggle("show");
+//   popup.parentElement.classList.toggle("popupOpened");
+// }
+
+$('.popup').on('click touchend', function() {
+  $(this).find('.popuptext').toggleClass('show');
+  $(this).toggleClass('popupOpened');
+});
 
 // Adding parallax effect for background images
 $('.hero').parallax({
